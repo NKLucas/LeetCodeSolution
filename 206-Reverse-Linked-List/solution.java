b20a9db8825f0188335d19d8bf3c79c991ff6b3e
NKLucas <lizhiyu1991@gmail.com>
2016-04-20 02:35:50 -0700

    [206. Reverse Linked List][Accepted] Prev and Next ListNode Pointer

---
+++ 206-Reverse-Linked-List/solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        
+        ListNode prev = null;
+        ListNode next = null;
+        while(head != null){
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
