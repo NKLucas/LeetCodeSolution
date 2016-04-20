e8d138efa808ecade2e637d281b3977a2634499d
NKLucas <lizhiyu1991@gmail.com>
2016-04-20 00:39:55 -0700

    [114. Flatten Binary Tree to Linked List][Accepted] Recursive flat left and right

--- 114-Flatten-Binary-Tree-to-Linked-List/solution.java
+++ 114-Flatten-Binary-Tree-to-Linked-List/solution.java
@@ -1,26 +1,35 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def flatten(self, root):
-        """
-        :type root: TreeNode
-        :rtype: void Do not return anything, modify root in-place instead.
-        """
-        if root is None:
-            return None
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+        // handling null root.
+        if (root == null){
+            return;
+        }
         
-        self.flatten(root.left)
-        self.flatten(root.right)
-        # swap left and right son to make sure the finall tree is right skewed.
-        root.left, root.right = root.right, root.left
-        rightSon = root
-        while rightSon.right != None:
-            rightSon = rightSon.right
-        rightSon.right = root.left
-        root.left = None
+        // handling leaf node.
+        if (root.left == null && root.right == null){
+            return;
+        }
         
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode left = root.left;
+        if (left != null){
+            while(left.right != null){
+                left = left.right;
+            }
+            left.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+        
+    }
+}
