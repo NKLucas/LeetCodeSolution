e6e925d8f24f2250486e2d54be94b2c60845e8ec
NKLucas <lizhiyu1991@gmail.com>
2016-04-20 16:19:32 -0700

    [155. Min Stack][Compile Error] Two Stacks

---
+++ 155-Min-Stack/solution.java
@@ -0,0 +1,27 @@
+class MinStack {
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> minStack = new Stack<>();
+    
+    public void push(int x) {
+        stack.push(x);
+        if (!minStack.isEmpty() && x > minStack){
+            minStack.push(minStack.peek());
+        } else {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+        
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
