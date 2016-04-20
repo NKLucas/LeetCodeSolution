b4d2eda48ebd06dc1fa8dfd5a4811d64a0f4ef6d
NKLucas <lizhiyu1991@gmail.com>
2016-04-20 16:37:50 -0700

    [231. Power of Two][Accepted] Bit Manipulate

---
+++ 231-Power-of-Two/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0){
+            return false;
+        }
+        
+        while (n > 1){
+            if ( (n & 1) == 1){
+                return false;
+            } else {
+                n >>= 1;
+            }
+        }
+        return true;
+        
+    }
+}
