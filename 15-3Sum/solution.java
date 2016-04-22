941daa9f01c7b1a3e53d3155fd92b90ef8046cee
NKLucas <lizhiyu1991@gmail.com>
2016-04-21 19:28:57 -0700

    [15. 3Sum][Accepted] Sort, Two Pointer, Skip Duplicates

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -5,11 +5,40 @@ public class Solution {
             return result;
         }
         
-        
         // sort the arrays before we search. Takes O(nlogn) time
         Arrays.sort(nums);
         
-        for (int i = 0; i < nums.length; )
-        
+        // get one and find the target = 0 - nums[i] by two pointers.
+        for (int i = 0; i < nums.length; i++){
+            if (i != 0 && nums[i] == nums[i-1]){
+                continue;
+            } else {
+                int target = 0 - nums[i];
+                int left = i + 1;
+                int right = nums.length - 1;
+                while(left < right){
+                    if (nums[left] + nums[right] == target){
+                        List<Integer> temp = new LinkedList<Integer>();
+                        temp.add(nums[i]);
+                        temp.add(nums[left]);
+                        temp.add(nums[right]);
+                        result.add(temp);
+                        left += 1;
+                        right -= 1;
+                        while (left < right && nums[left] == nums[left - 1]) { // to skip duplicates
+						    left++;
+					    }
+					    while (left < right && nums[right] == nums[right + 1]) { // to skip duplicates
+						    right--;
+					    }
+                    } else if (nums[left] + nums[right] > target){
+                        right -= 1;
+                    } else {
+                        left += 1;
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
