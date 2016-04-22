c8dff35c9d2c281e630a36cbd5aefd94d1c208ab
NKLucas <lizhiyu1991@gmail.com>
2016-04-21 18:05:00 -0700

    [49. Group Anagrams][Accepted] Functions to hash the anagram with same hashcode.

---
+++ 49-Group-Anagrams/solution.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // This problem requires to write a hash function that make all the anagrams have sam hashcode.
+        List<List<String>> result = new LinkeList<List<String>>();
+        HashMap<Integer, LinkedList<String>> map = new HashMap<>();
+        
+        // group the strings together based on the designed getHashCode function.
+        for (String s : strs){
+            int[] charCount = new int[26];
+            for (int i = 0; i < s.length(); i++){
+                int index = s.charAt(i) - 'a';
+                charCount[index] += 1;
+            }
+            int hashCode = getHashCode(charCount);
+            if (map.containsKey(hashCode)){
+                map.get(hashCode).add(s);
+            } else {
+                LinkedList<String> list = new LinkedList<>();
+                list.add(s);
+                map.put(hashCode, list);
+            }
+        }
+        
+        for(int code : map.keySet()){
+            result.add(map.get(code));
+        }
+        return result;
+    }
+    
+    private int getHashCode(int[] charCount){
+        int a = 33;
+        int b = 31;
+        int hashCode = 0;
+        for (int c : charCount){
+            
+        }
+    }
+    
+    
+    
+}
