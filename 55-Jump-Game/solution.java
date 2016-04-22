4e18d84d55036e8fec410c42699fa89d9378c9fc
NKLucas <lizhiyu1991@gmail.com>
2016-04-21 18:58:45 -0700

    [55. Jump Game][Compile Error]committed by LeetCode Extension

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -1,13 +1,16 @@
 public class Solution {
     public boolean canJump(int[] nums) {
-        int rightMost = 0;
+        boolean[] canReach = new int[nums.length];
+        canReach[0] = true;
         for(int i = 0; i < nums.length; i++){
-            if (i <= rightMost){
-                rightMost = Math.max(rightMost, i + nums[i]);
-            } else {
-                return false;
+            if (canReach[i]) {
+                for(int j = i + nums[i]; j > i; j--){
+                    if (j < nums.length){
+                        canReach[i] = true;
+                    }
+                }
             }
         }
-        return true;
+        return canReach[nums.length];
     }
 }
