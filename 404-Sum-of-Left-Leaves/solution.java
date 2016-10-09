890e0cc6f9fa808101ee724ef7a0bb7aaeaac8b8
NKLucas <lizhiyu1991@gmail.com>
2016-10-09 15:06:26 -0700

    [404. Sum of Left Leaves][Accepted]

---
+++ 404-Sum-of-Left-Leaves/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+    
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int result = 0;
+        
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++){
+                TreeNode now = q.poll();
+                if (now.left != null) {
+                    q.offer(now.left);
+                }
+                if (now.right != null) {
+                    q.offer(now.right);
+                }
+                if (now.left != null && now.left.left == null && now.left.right == null){
+                    result += now.left.val;
+                }
+            }
+        }
+        return result;
+    }
+    
+}
